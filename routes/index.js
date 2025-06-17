00727bbd2d3964cfdae4e9855d96620c6e73d14a
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-17 20:08:46 +0530

    feat: implement POST route for note insertion in index

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const route = express.Router();
 const db = require("../db");
+const pool = require("../db");
 
 route.use((req, res, next) => {
   console.log("Index Middleware");
@@ -9,6 +10,20 @@ route.use((req, res, next) => {
 
 route.get("/", (req, res) => {
   res.render("index");
+});
+
+route.post("/", (req, res) => {
+  const { noteHeading, noteDescription } = req.body;
+  const val = [noteHeading, noteDescription];
+
+  const sql = `INSERT INTO note (noteHeading, noteDesc) VALUES (?, ?)`;
+  pool.query(sql, val, (err, results, fields) => {
+    if (err) {
+      console.log("Database insertion failed", err.message);
+      return res.status(500).send("Insertion Failed");
+    }
+    console.log("Insertion successful", results.insertId);
+  });
 });
 
 module.exports = route;
