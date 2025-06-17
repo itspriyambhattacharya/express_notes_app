4ac332660b117342d323f04b91c911330cac9880
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-17 20:53:59 +0530

    feat: implement delete route for notes with middleware logging

--- routes/delete.js
+++ routes/delete.js
@@ -2,4 +2,24 @@ const express = require("express");
 const route = express.Router();
 const pool = require("../db");
 
+route.use((req, res, next) => {
+  console.log("Delete Middleware");
+  next();
+});
+
+route.get("/:id", (req, res) => {
+  console.log(req.params);
+  const delId = req.params.id;
+  const sql = `DELETE FROM note WHERE id = ?`;
+
+  pool.query(sql, [delId], (err, results, fields) => {
+    if (err) {
+      console.log("Deletion Failed");
+      return res.status(500).send("Failed to delete");
+    }
+    console.log(`Deleted note with id = ${delId}`);
+    res.redirect("/");
+  });
+});
+
 module.exports = route;
